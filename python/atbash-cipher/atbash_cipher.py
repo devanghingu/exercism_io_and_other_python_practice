41145fbda4263d0062c3af0eeb0a34e758269038
devanghingu <43535635+devanghingu@users.noreply.github.com>
2020-04-03 19:57:03 +0530

    3-apr-2020

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,8 +1,25 @@
-def encode(plain_text):
-    pass
+from string import ascii_lowercase
+atbash_table = str.maketrans(ascii_lowercase, ascii_lowercase[::-1]) # return all char table 
 
+
+def translate(text):
+    return_text=[]
+    for c in text.lower():
+        if c.isalnum():
+            return_text.append(c)
+    return ''.join(return_text).translate(atbash_table)
+
+def encode(plain_text):
+    print(atbash_table)
+    
+    cipher_text = translate(plain_text)
+    return ' '.join(cipher_text[i:i + 5] for i in range(0, len(cipher_text), 5))
 
 def decode(ciphered_text):
-    
+    return translate(ciphered_text)
 
-decode('asdf')
+
+
+
+
+
